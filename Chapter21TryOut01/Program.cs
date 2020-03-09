d16bc02749a5bc9af6b6c2b8ccc33e2a5d72349b
FabioVeiga <droidbinho@gmail.com>
2020-03-09 19:03:39 -0400

    Commit

---
+++ Chapter21TryOut01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Chapter21TryOut01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                FileStream aFile = new FileStream("Log.txt", FileMode.OpenOrCreate);
+                StreamWriter sw = new StreamWriter(aFile);
+
+                bool truth = true;
+                //Write data to file
+                sw.WriteLine("Hello to you");
+                sw.WriteLine("It is now {0} and things are looking good", DateTime.Now.ToString());
+                sw.Write("More than that");
+                sw.Write("It's {0} that C# is fun.", truth);
+                sw.Close();
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An exception has been thrrow");
+                Console.WriteLine(e.ToString());
+                Console.ReadLine();
+                return;
+            }
+        }
+    }
+}
---
+++ Chapter21TryOut02/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Chapter21TryOut02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            try
+            {
+                FileStream aFile = new FileStream("Log.txt", FileMode.Open);
+                StreamReader sr = new StreamReader(aFile);
+                //Read a first line
+                line = sr.ReadLine();
+                //Read data in line by line
+                while (line != null)
+                {
+                    Console.WriteLine(line);
+                    line = sr.ReadLine();
+                }
+            }catch(IOException e)
+            {
+                Console.WriteLine("An Exception has been thrown!");
+                Console.WriteLine(e.ToString());
+                return;
+            }
+            Console.ReadKey();
+        }
+    }
+}
